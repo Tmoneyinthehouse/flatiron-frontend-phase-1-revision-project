37d5adacbc57aa2a2b23b765c625c2ce4da5299d
Tyler Brown <tylerbrown487@gmail.com>
2023-08-07 12:00:46 -0400

    added functions to display cards and fetch data to index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 console.log("connected");
 
+async function fetchData("") {
+  try {
+    const resp = await fetch("");
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
+
 function createPlayerCard(player) {
   const card = document.createElement("div");
   card.classList.add("player-card");
@@ -22,4 +33,17 @@ function createPlayerCard(player) {
   card.appendChild(reboundsPerGame);
 
   return card;
+}
+
+async function displayPlayerCards() {
+  const url = 'db.json';
+  const playerContainer = document.getElementById('playerContainer');
+
+  try {
+    const data = await fetchData("");
+    data.players.forEach(player => {
+      const newPlayerCard = createPlayerCard(player);
+      playerContainer.appendChild(newPlayerCard);
+    });
+  }
 }
