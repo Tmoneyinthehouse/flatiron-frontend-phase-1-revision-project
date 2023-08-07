179a89d1e86b390dc361cfefd536a826a67696a5
Tyler Brown <tylerbrown487@gmail.com>
2023-08-06 22:15:06 -0400

    added function to create player elements in index.js and updated layout of json data in db.json

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("connected");
+
+function createPlayerCard(player) {
+  const card = document.createElement("div");
+  card.classList.add("player-card");
+
+  const playerName = document.createElement("h2");
+  playerName.textContent = player.name;
+
+  const pointsPerGame = document.createElement("p");
+  pointsPerGame.textContent = `Points Per Game: ${player.pointspergame}`;
+
+  const assistsPerGame = document.createElement("p");
+  assistsPerGame.textContent = `Assists Per Game: ${player.assistspergame}`;
+
+  const reboundsPerGame = document.createElement("p");
+  reboundsPerGame.textContent = `Rebounds Per Game: ${player.reboundspergame}`;
+
+  card.appendChild(playerName);
+  card.appendChild(pointsPerGame);
+  card.appendChild(assistsPerGame);
+  card.appendChild(reboundsPerGame);
+
+  return card;
+}
